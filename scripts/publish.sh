b8c3d0c198c23a976b215c868bafb841cff982e1
James Campbell <jcampbell@anduril.com>
2021-06-11 10:51:24 -0400

    updates to public

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -13,8 +13,10 @@ git commit -m 'updates to public'
 git push --recurse-submodules=on-demand
 git checkout gh-pages
 cd ..
+echo $PWD
 cp -R /tmp/public/* .
 git add -A 
 git commit -m 'updated gh-pages'
 git push --recurse-submodules=on-demand
 echo "done"
+git checkout main

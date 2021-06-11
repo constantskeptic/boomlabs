a9446adb449b63af2ef7902ca53eea5676f8eb7f
James Campbell <jcampbell@anduril.com>
2021-06-11 10:38:49 -0400

    updqates

---
+++ scripts/publish.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# move to root
+cd ../hugolanding
+# generate public content
+hugo -D
+# prep copy folder
+rm -rf /tmp/public && mkdir -p /tmp/public
+# copy out of git shit
+cp -R public/* /tmp/public
+# git yolo everything
+git add -A 
+git commit -m 'updates to public'
+git push --recurse-submodules=on-demand
+git checkout gh-pages
+cd ..
+cp -R /tmp/public/* .
+git add -A 
+git commit -m 'updated gh-pages'
+git push --recurse-submodules=on-demand
+echo "done"
